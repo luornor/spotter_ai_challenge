4d6ecd1b2324510f0f793e50edab063bea67659b
luornor <tetteynathan89@gmail.com>
2025-09-16 21:26:56 +0000

    modified:   frontend/src/pages/Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,10 +3,6 @@ import TripForm from "../components/TripForm";
 export default function Home() {
   return (
     <div className="container">
-      <div style={{display:'flex', justifyContent:'space-between', alignItems:'baseline'}}>
-        <h1 className="page-title">Trip Planner</h1>
-        <a className="nav-link" href="/">Home</a>
-      </div>
       <TripForm />
     </div>
   )
